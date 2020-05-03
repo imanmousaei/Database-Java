d0a3f49d372589c32d6d37485e1b9aa1cf114250
Iman Mousaei <imanmousaei1379@gmail.com>
2020-05-03 22:20:05 +0430

    changed Name from Commands to Strings

---
+++ src/io/Strings.java
@@ -0,0 +1,26 @@
+package io;
+
+public class Strings {
+    // Commands :
+    public static final String CREATE_TABLE = "Create Table";
+    public static final String INSERT = "Insert";
+    public static final String EDIT = "Edit";
+    public static final String DELETE = "Delete";
+
+
+    public static final String COMMAND = "Command";
+    public static final String TABLE = "Table";
+    public static final String TYPE = "type";
+    public static final String LENGTH = "len";
+    public static final String COLUMNS = "Columns";
+    public static final String COLUMN_NAME = "columnName";
+
+    // Types :
+    public static final String STRING = "String";
+    public static final String INT = "int";
+    public static final String DOUBLE = "double";
+    public static final String FLOAT = "float";
+    public static final String CHAR = "char";
+
+
+}
