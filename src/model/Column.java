e1515278b84d42e2a59e6f418e199886565120ad
Iman Mousaei <imanmousaei1379@gmail.com>
2020-05-03 21:52:40 +0430

    constructor, getter, setter added

--- src/model/Column.java
+++ src/model/Column.java
@@ -4,4 +4,39 @@ public class Column{
     private String name;
     private String type;
     private int size;
+
+    public Column(String name, String type, int size) {
+        this.name = name;
+        this.type = type;
+        this.size = size;
+    }
+    public Column(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
