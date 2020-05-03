edda49674a31725fbc67f76a06ddd0b644f5d3f8
Iman Mousaei <imanmousaei1379@gmail.com>
2020-05-03 18:33:17 +0430

    file io

---
+++ src/io/FileIO.java
@@ -0,0 +1,10 @@
+package io;
+
+public class FileIO {
+    public static void createFolder(String folderName){
+        // TODO
+    }
+    public static void createFile(String fileName){
+
+    }
+}
